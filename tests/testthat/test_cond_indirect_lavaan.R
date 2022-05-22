4d3baa74d8db67efc1cfdab4eb05a7db50be2273
Shu Fai Cheung <shufai.cheung@gmail.com>
2022-05-22 22:19:23 +0800

    Update test_cond_indirect_lavaan.R
    Add moderation only and mediation only tests.

--- tests/testthat/test_cond_indirect_lavaan.R
+++ tests/testthat/test_cond_indirect_lavaan.R
@@ -15,6 +15,8 @@ boot_out <- fit2boot_out(fit_boot)
 boot_est <- lapply(boot_out, function(x) x$est)
 boot_implied_stats <- lapply(boot_out, function(x) x$implied_stats)
 
+# Moderated mediation
+
 wv <- c(w1 = 5, w4 = 3)
 
 out <- cond_indirect(x = "x", y = "y",
@@ -40,8 +42,93 @@ out_boot_chk <- mapply(indirect,
                                        wvalues = wv),
                        SIMPLIFY = FALSE)
 
+# Mediation only
+
+dat <- modmed_x1m3w4y1
+modm <-
+"
+m1 ~ x
+m2 ~ m1
+m3 ~ m1 + m2
+y  ~ m3 + x
+"
+fitm <- sem(modm, dat, meanstructure = TRUE, fixed.x = FALSE, se = "none", baseline = FALSE)
+set.seed(85701)
+fitm_boot <- sem(modm, dat, meanstructure = TRUE, fixed.x = FALSE,
+                se = "bootstrap", bootstrap = 50, baseline = FALSE,
+                h1 = FALSE)
+bootm_out <- fit2boot_out(fitm_boot)
+bootm_est <- lapply(bootm_out, function(x) x$est)
+bootm_implied_stats <- lapply(bootm_out, function(x) x$implied_stats)
+
+outm <- cond_indirect(x = "m1", y = "m3",
+                     m = c("m2"),
+                     fit = fitm)
+outm_chk <- indirect(x = "m1", y = "m3",
+                    m = c("m2"),
+                    est = parameterEstimates(fitm))
+
+outm_boot <- cond_indirect(x = "m1", y = "m3",
+                     m = c("m2"),
+                     fit = fitm_boot,
+                     boot_ci = TRUE)
+outm_boot_chk <- mapply(indirect,
+                       est = bootm_est,
+                       implied_stats = bootm_implied_stats,
+                       MoreArgs = list(x = "m1",
+                                       y = "m3",
+                                       m = c("m2")),
+                       SIMPLIFY = FALSE)
+
+# Moderation only
+
+dat <- modmed_x1m3w4y1
+modmo <-
+"
+m1 ~ x
+m2 ~ m1
+m3 ~ m1 + m2 + m2:m1
+y  ~ m3 + x
+"
+fitmo <- sem(modmo, dat, meanstructure = TRUE, fixed.x = FALSE, se = "none", baseline = FALSE)
+set.seed(85701)
+fitmo_boot <- sem(modmo, dat, meanstructure = TRUE, fixed.x = FALSE,
+                se = "bootstrap", bootstrap = 50, baseline = FALSE,
+                h1 = FALSE)
+bootmo_out <- fit2boot_out(fitmo_boot)
+bootmo_est <- lapply(bootmo_out, function(x) x$est)
+bootmo_implied_stats <- lapply(bootmo_out, function(x) x$implied_stats)
+
+outmo <- cond_indirect(x = "m2", y = "m3", wvalues = c(m1 = -5),
+                     fit = fitmo)
+outmo_chk <- indirect(x = "m2", y = "m3", wvalues = c(m1 = -5),
+                    est = parameterEstimates(fitmo))
+
+outmo_boot <- cond_indirect(x = "m2", y = "m3", wvalues = c(m1 = -5),
+                     fit = fitmo_boot,
+                     boot_ci = TRUE)
+outmo_boot_chk <- mapply(indirect,
+                       est = bootmo_est,
+                       implied_stats = bootmo_implied_stats,
+                       MoreArgs = list(x = "m2",
+                                       y = "m3",
+                                       wvalues = c(m1 = -5)),
+                       SIMPLIFY = FALSE)
+
 test_that("cond_indirect: lavaan", {
     expect_identical(out$indirect, out_chk$indirect)
     expect_identical(out_boot$boot_indirect,
                      sapply(out_boot_chk, function(x) x$indirect))
+  })
+
+test_that("cond_indirect: lavaan, mediation only", {
+    expect_identical(outm$indirect, outm_chk$indirect)
+    expect_identical(outm_boot$boot_indirect,
+                     sapply(outm_boot_chk, function(x) x$indirect))
+  })
+
+test_that("cond_indirect: lavaan, moderation only", {
+    expect_identical(outmo$indirect, outmo_chk$indirect)
+    expect_identical(outmo_boot$boot_indirect,
+                     sapply(outmo_boot_chk, function(x) x$indirect))
   })
