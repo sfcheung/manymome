3a90049f5ae630411813446e9b14fc2f08e5100d
Shu Fai Cheung <shufai.cheung@gmail.com>
2022-07-25 09:59:29 +0800

    Add a test of indirect() for 3-way interaction
    Tests passed.

---
+++ tests/testthat/test_indirect_kway.R
@@ -0,0 +1,42 @@
+library(stdmodsem)
+
+dat <- data_med_mod_b_mod
+lm_m <- lm(m ~ x*w1 + c1 + c2, dat)
+lm_y <- lm(y ~ w1*m*w2 + x + c1 + c2, dat)
+lm_list <- lm2list(lm_m, lm_y)
+suppressMessages(library(lavaan))
+dat$w1x <- dat$w1 * dat$x
+dat$w1m <- dat$w1 * dat$m
+dat$w2m <- dat$w2 * dat$m
+dat$w1w2 <- dat$w1 * dat$w2
+dat$w1mw2 <- dat$w1 * dat$m * dat$w2
+mod <-
+"
+m ~ a * x + af1 * w1 + ad1 * w1x + c1 + c2
+y ~ b * m + cp * x + bf1 * w1 + bf2 * w2 +
+    bd1 * w1m + bd2 * w2m + bd12 * w1w2 + be12 * w1mw2 + c1 + c2
+# Covariances not added. Not necessary for this test.
+"
+fit <- sem(mod, dat, fixed.x = FALSE, warn = FALSE)
+est <- parameterEstimates(fit)
+
+wvalues <- c(w1 = 5, w2 = -4)
+
+# Moderated-moderated mediation
+ce_1b_chk <- indirect(x = "x", y = "y", m = "m",
+                      fit = fit,
+                      wvalues = wvalues)
+ce_1b_chk2 <- (est[est$label == "a", "est"] +
+                wvalues["w1"] * est[est$label == "ad1", "est"]) *
+              (est[est$label == "b", "est"] +
+                wvalues["w1"] * est[est$label == "bd1", "est"] +
+                wvalues["w2"] * est[est$label == "bd2", "est"] +
+                wvalues["w1"] * wvalues["w2"] * est[est$label == "be12", "est"])
+
+test_that("check indirect: 3-way", {
+    expect_equal(
+        ce_1b_chk$indirect,
+        ce_1b_chk2,
+        ignore_attr = TRUE
+      )
+  })
