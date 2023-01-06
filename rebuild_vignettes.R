d18d090c3dd89d0f945f828454501ef9914c37f2
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-01-07 00:10:34 +0800

    Use precomputed results to speed up vignettes buiilding
    Tested by pkgdown::build_site()

---
+++ rebuild_vignettes.R
@@ -0,0 +1,12 @@
+# Adapted from https://www.kloppenborg.ca/2021/06/long-running-vignettes/
+
+base_dir <- getwd()
+
+setwd("vignettes/")
+knitr::knit("do_boot.Rmd.originaL", output = "do_boot.Rmd")
+knitr::knit("manymome.Rmd.originaL", output = "manymome.Rmd")
+knitr::knit("med_lav.Rmd.originaL", output = "med_lav.Rmd")
+knitr::knit("med_lm.Rmd.originaL", output = "med_lm.Rmd")
+knitr::knit("mome_lm.Rmd.original", output = "mome_lm.Rmd")
+
+setwd(base_dir)
