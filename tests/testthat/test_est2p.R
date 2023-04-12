e9a5e45f7a1a044824cd71b1a20d46df7dacf5f1
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-04-13 00:51:22 +0800

    Add est2p() for asymmetric bootstrap p-value
    Tests passed.

---
+++ tests/testthat/test_est2p.R
@@ -0,0 +1,27 @@
+library(testthat)
+library(manymome)
+
+set.seed(59876)
+n <- 5000
+x1 <- rnorm(n, 2, 4)
+x2 <- rnorm(n, 2, 1)
+x3 <- rnorm(n, -2, 4)
+x4 <- rnorm(n, -2, 1)
+x5 <- rnorm(n, 0, 1)
+xNA <- rep(NA, n)
+x11 <- rnorm(99, 2, 1)
+
+test_that("est2p", {
+    expect_equal(est2p(x1), 0.6228)
+    expect_equal(est2p(x2), 0.0432)
+    expect_equal(est2p(x3), 0.6452)
+    expect_equal(est2p(x4), 0.0456)
+    expect_equal(est2p(x5), 0.9796)
+    expect_equal(est2p(-x2), 0.0432)
+    expect_equal(est2p(-x4), 0.0456)
+    expect_equal(est2p(-x3), 0.6452)
+    expect_equal(est2p(-x5), 0.9796)
+    expect_equal(est2p(xNA), NA)
+    expect_equal(suppressWarnings(est2p(x11)), NA)
+    expect_warning(est2p(x11))
+  })
