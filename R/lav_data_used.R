37c0ec8b07a72a200f01bb93e66eae62a09ff1a1
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-05-01 11:25:07 +0800

    Update lav_data_used for lavaan.mi objects
    Tests and checks passed.

--- R/lav_data_used.R
+++ R/lav_data_used.R
@@ -19,6 +19,28 @@
 
 lav_data_used <- function(fit,
                           drop_colon = TRUE) {
+    type <- NA
+    if (inherits(fit, "lavaan")) {
+        type <- "lavaan"
+      }
+    if (inherits(fit, "lavaan.mi")) {
+        type <- "lavaan.mi"
+      }
+    if (isTRUE(is.na(type))) {
+        stop("Object is not of a supported type.")
+      }
+    out <- switch(type,
+                  lavaan = lav_data_used_lavaan(fit = fit,
+                                                drop_colon = drop_colon),
+                  lavaan.mi = lav_data_used_lavaan_mi(fit = fit,
+                                                      drop_colon = drop_colon))
+    out
+  }
+
+#' @noRd
+
+lav_data_used_lavaan <- function(fit,
+                                 drop_colon = TRUE) {
     dat <- lavaan::lavInspect(fit, "data")
     i_excluded <- lavaan::lavInspect(fit, "empty.idx")
     vnames <- colnames(dat)
@@ -33,4 +55,68 @@ lav_data_used <- function(fit,
       } else {
         return(dat)
       }
+  }
+
+#' @noRd
+
+lav_data_used_lavaan_mi <- function(fit,
+                                    drop_colon = TRUE) {
+    dat_list <- fit@DataList
+    ovnames <- lavaan::lavNames(fit, "ov")
+    fit_org <- lavaan_from_lavaam_mi(fit)
+    dat_common <- keep_identical_cells(dat_list)
+    # dat_common <- dat_common[, colnames(dat_common) %in% ovnames]
+    fit_common <- lavaan::lavaan(model = fit_org,
+                                 data = dat_common,
+                                 missing = "fiml.x",
+                                 do.fit = FALSE)
+    dat <- lavaan::lavInspect(fit_common, "data")
+    i_excluded <- lavaan::lavInspect(fit_common, "empty.idx")
+    vnames <- colnames(dat)
+    if (drop_colon) {
+        vraw <- vnames[!grepl(":", colnames(dat))]
+        vcolon <- apply(expand.grid(vraw, vraw), 1, paste0, collapse = ":")
+        vkeep <- vnames[!(vnames %in% vcolon)]
+        dat <- dat[, vkeep]
+      }
+    if (length(i_excluded) > 0) {
+        return(dat[-i_excluded, ])
+      } else {
+        return(dat)
+      }
+  }
+
+#' Compare two data sets and set to NA for discrepancy
+#' @noRd
+
+keep_identical_cells_i <- function(x1, x2) {
+    a <- x1 != x2
+    a[is.na(a)] <- FALSE
+    p <- ncol(x1)
+    for (i in seq_len(p)) {
+        x1[a[, i, drop = TRUE], i] <- NA
+      }
+    x1
+  }
+
+#' Compare data sets and keep only values identical across data sets.
+#' @noRd
+
+keep_identical_cells <- function(x) {
+    out <- Reduce(keep_identical_cells_i, x)
+    out
+  }
+
+#' Create a lavaan object based on a lavaan.mi object
+#' @noRd
+
+lavaan_from_lavaam_mi <- function(fit_mi) {
+    fit_tmp <- methods::new("lavaan",
+                  version = as.character(utils::packageVersion("lavaan")))
+    fit_tmp@Model <- fit_mi@Model
+    fit_tmp@Data <- fit_mi@Data
+    fit_tmp@ParTable <- fit_mi@ParTableList[[1]]
+    fit_tmp@pta <- fit_mi@pta
+    fit_tmp@Options <- fit_mi@Options
+    fit_tmp
   }
--- tests/testthat/test_lav_data_used_mi.R
+++ tests/testthat/test_lav_data_used_mi.R
@@ -10,7 +10,7 @@ dat <- modmed_x1m3w4y1
 n <- nrow(dat)
 p <- ncol(dat)
 pstar <- n * p
-q <- 10
+q <- 100
 set.seed(51453)
 tmp <- sample(pstar, q)
 tmp2 <- list(i = row(matrix(NA, n, p))[tmp],
@@ -26,7 +26,7 @@ m2 ~ a2 * m1 + m1:w2 + w2
 m3 ~ a3 * m2
 y  ~ a4 * m3 + c4 * x + m3:w4 + w4
 "
-fit1_lav <- sem(mod, dat, meanstructure = TRUE, fixed.x = FALSE, se = "none", baseline = FALSE)
+fit1_lav <- sem(mod, dat, meanstructure = TRUE, fixed.x = FALSE, se = "none", baseline = FALSE, missing = "fiml", warn = FALSE)
 
 mod2 <-
 "
@@ -53,12 +53,56 @@ fit2_mi <- sem.mi(mod2, dat_mi,
                   h1 = FALSE,
                   warn = FALSE)
 
+dat_lv <- simple_mediation_latent
+n <- nrow(dat_lv)
+p <- ncol(dat_lv)
+pstar <- n * p
+q <- 100
+set.seed(51453)
+tmp <- sample(pstar, q)
+tmp2 <- list(i = row(matrix(NA, n, p))[tmp],
+             j = col(matrix(NA, n, p))[tmp])
+for (ii in seq_len(q)) {
+    dat_lv[tmp2$i[ii], tmp2$j[ii]] <- NA
+  }
+
+mod_lv <-
+"
+fx =~ x1 + x2 + x3
+fm =~ m1 + m2 + m3
+fy =~ y1 + y2 + y3
+fm ~ a * fx
+fy ~ b * fm + cp * fx
+indirect := a * b
+"
+fit_lv_lav <- sem(mod_lv, dat_lv,
+                  meanstructure = TRUE,
+                  missing = "fiml.x",
+                  baseline = FALSE,
+                  h1 = FALSE,
+                  warn = FALSE)
+
+set.seed(235413)
+dat_lv_mi <- amelia(dat_lv, m = 5)$imputations
+
+fit_lv_mi <- sem.mi(mod_lv, dat_lv_mi,
+                    meanstructure = TRUE,
+                    missing = "fiml.x",
+                    baseline = FALSE,
+                    h1 = FALSE,
+                    warn = FALSE)
+
 test_that("lav_data_used with mi", {
-    expect_equal(nrow(lav_data_used(fit)), 200)
-    expect_equal(nrow(lav_data_used(fitm)), 198)
-    expect_equal(ncol(lav_data_used(fit)), 7)
-    expect_equal(ncol(lav_data_used(fitm)), 7)
-    expect_equal(ncol(lav_data_used(fit, drop_colon = FALSE)), 9)
-    expect_equal(ncol(lav_data_used(fitm, drop_colon = FALSE)), 9)
-    expect_equal(nrow(lav_data_used(fit2)), 199)
+    expect_true(all.equal(lav_data_used(fit1_mi),
+                          lav_data_used(fit1_lav)))
+    expect_true(all.equal(lav_data_used(fit1_mi, drop_colon = FALSE),
+                          lav_data_used(fit1_lav, drop_colon = FALSE)))
+    expect_true(all.equal(lav_data_used(fit2_mi),
+                          lav_data_used(fit2_lav)))
+    expect_true(all.equal(lav_data_used(fit2_mi, drop_colon = FALSE),
+                          lav_data_used(fit2_lav, drop_colon = FALSE)))
+    expect_true(all.equal(lav_data_used(fit_lv_mi),
+                          lav_data_used(fit_lv_lav)))
+    expect_true(all.equal(lav_data_used(fit_lv_mi, drop_colon = FALSE),
+                          lav_data_used(fit_lv_lav, drop_colon = FALSE)))
   })
