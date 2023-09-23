649fb5b0a44477716bf02cd7c88d624c1f15197d
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-09-23 15:34:48 +0800

    Add tests for bootstrapping CIs with lav-obs paths
    Tests and checks passed.

--- tests/testthat/test_indirect_simple_med_latent_observed.R
+++ tests/testthat/test_indirect_simple_med_latent_observed.R
@@ -165,3 +165,142 @@ test_that("indirect: latent variable and observed variables", {
     expect_equal(out_std_my$indirect,
                  std_my[13, "est.std"])
   })
+
+skip_on_cran()
+
+# Bootstrapping
+
+fit_x_boot <- sem(mod_x, dat,
+                  se = "bootstrap", bootstrap = 50, baseline = FALSE,
+                  h1 = FALSE, warn = FALSE,
+                  iseed = 85701)
+boot_x_out <- suppressMessages(fit2boot_out_do_boot(fit_x, R = 50, seed = 85701,
+                                                    progress = FALSE))
+boot_x_est <- lapply(boot_x_out, function(x) x$est)
+boot_x_implied_stats <- lapply(boot_x_out, function(x) x$implied_stats)
+
+out_x_cond_boot <- cond_indirect(x = "x", y = "fy", m = c("fm"), fit = fit_x,
+                                 boot_ci = TRUE,
+                                 boot_out = boot_x_out)
+out_x_cond_boot_chk <- mapply(indirect_i,
+                              est = boot_x_est,
+                              implied_stats = boot_x_implied_stats,
+                              MoreArgs = list(x = "x",
+                                              y = "fy",
+                                              m = "fm"),
+                              SIMPLIFY = FALSE)
+
+fit_m_boot <- sem(mod_m, dat,
+                  se = "bootstrap", bootstrap = 50, baseline = FALSE,
+                  h1 = FALSE, warn = FALSE,
+                  iseed = 85701)
+boot_m_out <- suppressMessages(fit2boot_out_do_boot(fit_m, R = 50, seed = 85701,
+                                                    progress = FALSE))
+boot_m_est <- lapply(boot_m_out, function(x) x$est)
+boot_m_implied_stats <- lapply(boot_m_out, function(x) x$implied_stats)
+
+out_m_cond_boot <- cond_indirect(x = "fx", y = "fy", m = c("m"), fit = fit_m,
+                                 boot_ci = TRUE,
+                                 boot_out = boot_m_out)
+out_m_cond_boot_chk <- mapply(indirect_i,
+                              est = boot_m_est,
+                              implied_stats = boot_m_implied_stats,
+                              MoreArgs = list(x = "fx",
+                                              y = "fy",
+                                              m = "m"),
+                              SIMPLIFY = FALSE)
+
+fit_y_boot <- sem(mod_y, dat,
+                  se = "bootstrap", bootstrap = 50, baseline = FALSE,
+                  h1 = FALSE, warn = FALSE,
+                  iseed = 85701)
+boot_y_out <- suppressMessages(fit2boot_out_do_boot(fit_y, R = 50, seed = 85701,
+                                                    progress = FALSE))
+boot_y_est <- lapply(boot_y_out, function(x) x$est)
+boot_y_implied_stats <- lapply(boot_y_out, function(x) x$implied_stats)
+
+out_y_cond_boot <- cond_indirect(x = "fx", y = "y", m = c("fm"), fit = fit_y,
+                                 boot_ci = TRUE,
+                                 boot_out = boot_y_out)
+out_y_cond_boot_chk <- mapply(indirect_i,
+                              est = boot_y_est,
+                              implied_stats = boot_m_implied_stats,
+                              MoreArgs = list(x = "fx",
+                                              y = "y",
+                                              m = "fm"),
+                              SIMPLIFY = FALSE)
+
+fit_xm_boot <- sem(mod_xm, dat,
+                   se = "bootstrap", bootstrap = 50, baseline = FALSE,
+                   h1 = FALSE, warn = FALSE,
+                   iseed = 85701)
+boot_xm_out <- suppressMessages(fit2boot_out_do_boot(fit_xm, R = 50, seed = 85701,
+                                                     progress = FALSE))
+boot_xm_est <- lapply(boot_xm_out, function(x) x$est)
+boot_xm_implied_stats <- lapply(boot_xm_out, function(x) x$implied_stats)
+
+out_xm_cond_boot <- cond_indirect(x = "x", y = "fy", m = c("m"), fit = fit_xm,
+                                 boot_ci = TRUE,
+                                 boot_out = boot_xm_out)
+out_xm_cond_boot_chk <- mapply(indirect_i,
+                              est = boot_xm_est,
+                              implied_stats = boot_xm_implied_stats,
+                              MoreArgs = list(x = "x",
+                                              y = "fy",
+                                              m = "m"),
+                              SIMPLIFY = FALSE)
+
+fit_xy_boot <- sem(mod_xy, dat,
+                  se = "bootstrap", bootstrap = 50, baseline = FALSE,
+                  h1 = FALSE, warn = FALSE,
+                  iseed = 85701)
+boot_xy_out <- suppressMessages(fit2boot_out_do_boot(fit_xy, R = 50, seed = 85701,
+                                                    progress = FALSE))
+boot_xy_est <- lapply(boot_xy_out, function(x) x$est)
+boot_xy_implied_stats <- lapply(boot_xy_out, function(x) x$implied_stats)
+
+out_xy_cond_boot <- cond_indirect(x = "x", y = "y", m = c("fm"), fit = fit_xy,
+                                 boot_ci = TRUE,
+                                 boot_out = boot_xy_out)
+out_xy_cond_boot_chk <- mapply(indirect_i,
+                              est = boot_xy_est,
+                              implied_stats = boot_xy_implied_stats,
+                              MoreArgs = list(x = "x",
+                                              y = "y",
+                                              m = "fm"),
+                              SIMPLIFY = FALSE)
+
+fit_my_boot <- sem(mod_my, dat,
+                  se = "bootstrap", bootstrap = 50, baseline = FALSE,
+                  h1 = FALSE, warn = FALSE,
+                  iseed = 85701)
+boot_my_out <- suppressMessages(fit2boot_out_do_boot(fit_my, R = 50, seed = 85701,
+                                                    progress = FALSE))
+boot_my_est <- lapply(boot_my_out, function(x) x$est)
+boot_my_implied_stats <- lapply(boot_my_out, function(x) x$implied_stats)
+
+out_my_cond_boot <- cond_indirect(x = "fx", y = "y", m = c("m"), fit = fit_my,
+                                 boot_ci = TRUE,
+                                 boot_out = boot_my_out)
+out_my_cond_boot_chk <- mapply(indirect_i,
+                              est = boot_my_est,
+                              implied_stats = boot_my_implied_stats,
+                              MoreArgs = list(x = "fx",
+                                              y = "y",
+                                              m = "m"),
+                              SIMPLIFY = FALSE)
+
+test_that("indirect: latent variable and observed variables, boot", {
+    expect_equal(out_x_cond_boot$boot_indirect,
+                 sapply(out_x_cond_boot_chk, function(x) x$indirect))
+    expect_equal(out_m_cond_boot$boot_indirect,
+                 sapply(out_m_cond_boot_chk, function(x) x$indirect))
+    expect_equal(out_y_cond_boot$boot_indirect,
+                 sapply(out_y_cond_boot_chk, function(x) x$indirect))
+    expect_equal(out_xm_cond_boot$boot_indirect,
+                 sapply(out_xm_cond_boot_chk, function(x) x$indirect))
+    expect_equal(out_xy_cond_boot$boot_indirect,
+                 sapply(out_xy_cond_boot_chk, function(x) x$indirect))
+    expect_equal(out_my_cond_boot$boot_indirect,
+                 sapply(out_my_cond_boot_chk, function(x) x$indirect))
+  })
