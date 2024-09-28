945e7d7953e63521fafe7241dcbb4d72740255cd
Shu Fai Cheung <shufai.cheung@gmail.com>
2024-09-28 12:32:51 +0800

    Alert users that cols and rows are limited to one variable

--- tests/testthat/test_plot_cond_indirect_effects_2ws.R
+++ tests/testthat/test_plot_cond_indirect_effects_2ws.R
@@ -51,3 +51,8 @@ test_that("Three moderators", {
   expect_no_error(plot(out_1, graph_type = "tumble", facet_grid_rows = "gp", facet_grid_cols = "w4"))
   expect_no_error(plot(out_1, graph_type = "tumble", facet_grid_rows = "w4", facet_grid_cols = "city"))
 })
+
+test_that("Three moderators", {
+  expect_error(plot(out_1, facet_grid_cols = c("gp", "w4")))
+  expect_error(plot(out_1, facet_grid_rows = c("gp", "w4")))
+})
