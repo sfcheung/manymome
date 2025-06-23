379e2b0a8a1fe6218b66b3f8b366292842e321dd
Shu Fai Cheung <shufai.cheung@gmail.com>
2025-06-23 21:05:38 +0800

    WIP

--- tests/testthat/test_q_fct_mediation_sem_lm2lav.R
+++ tests/testthat/test_q_fct_mediation_sem_lm2lav.R
@@ -54,16 +54,6 @@ mf_from_lm <- function(
   out
 }
 
-lm_1 <- lm_forms_simple[1]
-tmp <- mf_from_lm(
-            lm_1,
-            dat
-          )
-
-lm_mf <- lapply(lm_forms_simple,
-                mf_from_lm,
-                data = dat)
-
 mm_from_lm_forms <- function(
                       lm_forms,
                       data,
@@ -79,12 +69,12 @@ mm_from_lm_forms <- function(
     lm_all[[i]]$model <- stats::model.frame(
                             lm_all[[i]]$terms,
                             data = data,
-                            na.action = "na.pass"
+                            na.action = na.action
                           )
   }
   mm <- merge_model_matrix(lm_all)
   b_names <- sapply(lm_all,
-                    function(x) names(x$coefficients[-1]),
+                    function(x) names(x$coefficients),
                     USE.NAMES = TRUE,
                     simplify = FALSE)
   terms <- sapply(lm_all,
@@ -95,35 +85,32 @@ mm_from_lm_forms <- function(
                     function(x) summary(x)$coefficients,
                     USE.NAMES = TRUE,
                     simplify = FALSE)
-
+  model_matrices <- sapply(
+                  lm_all,
+                  function(x) {
+                    stats::model.matrix(
+                        attr(x$model, "terms"),
+                        x$model
+                    )
+                  },
+                  USE.NAMES = TRUE,
+                  simplify = FALSE
+                )
+  # TODO:
+  # - Removed duplicated objects
   list(model_matrix = mm,
+       model_matrices = model_matrices,
        b_names = b_names,
        terms = terms,
        coefficients = coefficients,
        lm_all = lm_all)
 }
 
-tmp <- mm_from_lm_forms(lm_forms_simple,
-                        data = dat)
-head(tmp$model_matrix)
-nrow(tmp$model_matrix)
-tmp$b_names
-tmp$coefficients
-
-tmp <- mm_from_lm_forms(lm_forms_serial,
-                        data = dat)
-head(tmp$model_matrix)
-nrow(tmp$model_matrix)
-
-tmp <- mm_from_lm_forms(lm_forms_parallel,
-                        data = dat)
-head(tmp$model_matrix)
-nrow(tmp$model_matrix)
-
 b_names_to_lavaan_model <- function(
                               x,
                               merge = TRUE) {
   tmpfct <- function(x_i, y_i) {
+    x_i <- x_i[-which(x_i == "(Intercept)")]
     paste0(y_i,
            " ~ ",
            paste0(x_i,
@@ -142,18 +129,202 @@ b_names_to_lavaan_model <- function(
   out0
 }
 
-tmp2 <- b_names_to_lavaan_model(tmp$b_names)
-cat(tmp2)
+lavaan_get_betas_etc <- function(dv, ivs_list, ptable) {
+    ptable_dv <- ptable[(ptable$lhs == dv) & (ptable$op == "~"), , drop = FALSE]
+    ivs <- ivs_list[[dv]]
+    betas <- ptable_dv[which(ptable_dv$rhs %in% ivs), , drop = FALSE]
+    rownames(betas) <- ivs
+    betas
+  }
 
-fit <- lavaan::sem(model = tmp2,
-                   data = tmp$model_matrix,
-                   missing = "fiml.x")
-parameterEstimates(fit,
-                   rsquare = TRUE)
+lavaan_get_intercepts_etc <- function(dv, ptable) {
+    ptable_dv <- ptable[(ptable$lhs == dv) & (ptable$op == "~1"), , drop = FALSE]
+    if (nrow(ptable_dv) == 0) {
+        out <- NULL
+      } else {
+        out <- ptable_dv
+      }
+    rownames(out) <- "(Intercept)"
+    out
+  }
 
-tmp$coefficients
+lavaan_get_rsq <- function(dv, ptable) {
+    ptable_dv <- ptable[(ptable$lhs == dv) & (ptable$op == "r2"), , drop = FALSE]
+    if (nrow(ptable_dv) == 0) {
+        out <- NULL
+      } else {
+        out <- ptable_dv[, "est", drop = TRUE]
+      }
+    names(out) <- dv
+    out
+  }
 
-lm_from_lavaan_list(fit)
+#' @noRd
+# A simplified version of lm_from_lavaan_list_i()
+# For q_* function
+lm_from_lavaan_list_for_q <- function(
+                                  fit,
+                                  mm,
+                                  ci_level = .95,
+                                  group_number = NULL
+                                ) {
+  # Assume it has only one group
+  ptable <- lavaan::parameterEstimates(fit,
+                                       standardized = TRUE,
+                                       level = .95,
+                                       rsquare = TRUE)
+  b_names <- mm$b_names
+  # Get all dvs (ov.nox, lv.ox)
+  dvs <- names(b_names)
+  # Get all ivs
+  ivs_list <- sapply(b_names,
+                     function(x) {
+                      x[-which(x == "(Intercept)")]
+                     },
+                     USE.NAMES = TRUE,
+                     simplify = FALSE)
+  # Get estimates and other statistics
+  bs_list <- sapply(dvs, lavaan_get_betas_etc,
+                    ivs_list = ivs_list,
+                    ptable = ptable,
+                    simplify = FALSE)
+  # Get all intercepts
+  int_list <- sapply(dvs, lavaan_get_intercepts_etc,
+                      ptable = ptable,
+                      simplify = FALSE)
+  # Get all R-squares
+  rsq_list <- sapply(dvs, lavaan_get_rsq,
+                      ptable = ptable,
+                      simplify = FALSE)
+  # Combine them
+  coefs_list <- mapply(function(x, y) {rbind(x, y)},
+                      x = int_list,
+                      y = bs_list,
+                      SIMPLIFY = FALSE)
+
+  # TODO:
+  # - Add CIs
+  # - Add betas
+
+  # lm_version
+  coefs_lm_list <- mapply(lm_coef_from_lavaan_i,
+                          lav_coefs = coefs_list,
+                          lm_coefs = mm$coefficients,
+                          USE.NAMES = TRUE,
+                          SIMPLIFY = FALSE)
+
+  # Get all product terms
+  # NOTE: No need because they are formed from lm()
+  # Generate lm_like_object
+  mods <- mapply(to_formula, dv = dvs, ivs = ivs_list)
+  # Return an lm_list object
+  out <- mapply(function(dv, model, ivs, coefs, coefs_lm, rsq) {
+                  list(
+                        dv = dv,
+                        model = model,
+                        ivs = ivs,
+                        coefs = coefs,
+                        coefs_lm = coefs_lm,
+                        rsquare = rsq
+                      )
+                },
+                dv = dvs,
+                model = mods,
+                ivs = ivs_list,
+                coefs = coefs_list,
+                rsq = rsq_list,
+                coefs_lm = coefs_lm_list,
+                SIMPLIFY = FALSE,
+                USE.NAMES = TRUE)
+  out
+}
+
+lm_coef_from_lavaan_i <- function(
+                        lav_coefs,
+                        lm_coefs
+                      ) {
+  out <- lm_coefs
+  i <- match(rownames(lav_coefs), rownames(out))
+  out[i, "Estimate"] <- lav_coefs$est
+  out[i, "Std. Error"] <- lav_coefs$se
+  out[i, "t value"] <- lav_coefs$z
+  out[i, "Pr(>|t|)"] <- lav_coefs$pvalue
+  colnames(out)[colnames(out) == "t value"] <- "z value"
+  colnames(out)[colnames(out) == "Pr(>|t|)"] <- "Pr(>|z|)"
+  out
+}
+
+lm_coef_from_lavaan <- function(
+                        coef_template,
+                        lm_from_lavaan
+                      ) {
+  coef_lav <- sapply(lm_from_lavaan,
+                     function(x) x$coefs,
+                     USE.NAMES = TRUE,
+                     simplify = FALSE)
+  out <- mapply(lm_coef_from_lavaan_i,
+                lav_coefs = coef_lav,
+                lm_coefs = coef_template,
+                SIMPLIFY = FALSE,
+                USE.NAMES = TRUE)
+  out
+}
+
+mm <- mm_from_lm_forms(
+            lm_forms_simple,
+            data = dat
+          )
+mod <- b_names_to_lavaan_model(mm$b_names)
+fit <- lavaan::sem(
+          model = mod,
+          data = mm$model_matrix,
+          missing = "fiml.x",
+          meanstructure = TRUE
+        )
+lm_from_lavaan <- lm_from_lavaan_list_for_q(
+                    fit = fit,
+                    mm = mm
+                  )
+lm_from_lavaan[[1]]
+
+# Select std.nox / std.all
+
+attr(mm$model_matrices[[1]], "assign")
+attr(mm$terms[[1]], "dataClasses")
+
+attributes(mm$model_matrices[[1]])
+attributes(mm$terms[[1]])
+
+numeric_ivs <- function(mm, y) {
+  # TODO:
+  # - Need to handle special terms, such as product terms
+  data_classes <- attr(mm$terms[[y]], "dataClasses")
+  term_labels <- attr(mm$terms[[y]], "term.labels")
+  mm_assign <- attr(mm$model_matrices[[y]], "assign")
+  b_names <- mm$b_names[[y]]
+  b_types <- b_names
+  names(b_types) <- b_names
+  j <- seq_along(term_labels)
+  for (xx in seq_along(b_types)) {
+    if (mm_assign[xx] %in% j) {
+      b_types[xx] <- data_classes[term_labels[mm_assign[xx]]]
+    }
+  }
+}
+
+###
+
+mf <- stats::model.frame(
+      lm_forms_simple[1],
+      data = dat,
+      na.action = "na.pass"
+    )
+mm <- model.matrix(
+        attr(mf, "terms"),
+        mf
+      )
+
+attributes(mm)
 
 })
 
