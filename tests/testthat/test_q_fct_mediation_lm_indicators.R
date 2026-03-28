942d1a0bfcf28901536289056499bbf5b6909844
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-03-28 20:04:19 +0800

    More tests
    tests and checks passed.

--- tests/testthat/test_q_fct_mediation_lm_indicators.R
+++ tests/testthat/test_q_fct_mediation_lm_indicators.R
@@ -29,6 +29,64 @@ out <- q_mediation(
           parallel = FALSE,
           progress = !is_testing())
 
+out_simple <- q_simple_mediation(
+          x = "x10",
+          y = "y",
+          m = "m",
+          cov = c("c2", "x12"),
+          indicators = list(y = c("-x01", "x02", "x03"),
+                            m = c("x04", "x05", "x09"),
+                            c2 = c("x11", "-x13", "x14")),
+          data = data_sem,
+          R = 200,
+          seed = 1234,
+          parallel = FALSE,
+          progress = !is_testing())
+
+out_parallel <- q_parallel_mediation(
+          x = "x10",
+          y = "y",
+          m = "m",
+          cov = c("c2", "x12"),
+          indicators = list(y = c("-x01", "x02", "x03"),
+                            m = c("x04", "x05", "x09"),
+                            c2 = c("x11", "-x13", "x14")),
+          data = data_sem,
+          R = 200,
+          seed = 1234,
+          parallel = FALSE,
+          progress = !is_testing())
+
+out_serial <- q_serial_mediation(
+          x = "x10",
+          y = "y",
+          m = "m",
+          cov = c("c2", "x12"),
+          indicators = list(y = c("-x01", "x02", "x03"),
+                            m = c("x04", "x05", "x09"),
+                            c2 = c("x11", "-x13", "x14")),
+          data = data_sem,
+          R = 200,
+          seed = 1234,
+          parallel = FALSE,
+          progress = !is_testing())
+
+out_user <- q_mediation(
+          x = "x10",
+          y = "y",
+          m = "m",
+          cov = c("c2", "x12"),
+          model = c("x10 -> m -> y",
+                    "x10 -> y"),
+          indicators = list(y = c("-x01", "x02", "x03"),
+                            m = c("x04", "x05", "x09"),
+                            c2 = c("x11", "-x13", "x14")),
+          data = data_sem,
+          R = 200,
+          seed = 1234,
+          parallel = FALSE,
+          progress = !is_testing())
+
 out_chk <- q_mediation(
           x = "x10",
           y = "y",
@@ -44,6 +102,26 @@ out_chk <- q_mediation(
 expect_identical(out$ind_total,
                  out_chk$ind_total)
 expect_identical(out$ind_out,
+                 out_chk$ind_out)
+
+expect_identical(out_simple$ind_total,
+                 out_chk$ind_total)
+expect_identical(out_simple$ind_out,
+                 out_chk$ind_out)
+
+expect_identical(out_parallel$ind_total,
+                 out_chk$ind_total)
+expect_identical(out_parallel$ind_out,
+                 out_chk$ind_out)
+
+expect_identical(out_serial$ind_total,
+                 out_chk$ind_total)
+expect_identical(out_serial$ind_out,
+                 out_chk$ind_out)
+
+expect_identical(out_user$ind_total,
+                 out_chk$ind_total)
+expect_identical(out_user$ind_out,
                  out_chk$ind_out)
 
 })
