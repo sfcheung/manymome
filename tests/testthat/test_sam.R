9b85049d984f07234de44ff8696b65c8705c974c
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-04-03 20:56:32 +0800

    More tests
    tests and checks passed.

--- tests/testthat/test_sam.R
+++ tests/testthat/test_sam.R
@@ -4,6 +4,145 @@ library(manymome)
 library(testthat)
 suppressMessages(library(lavaan))
 
+test_that("SAM: lavaan functions", {
+
+data_sem_miss <- data_sem
+data_sem_miss[1:10, 2:14] <- NA
+data_sem_miss[11:20, c(1:3, 5:14)] <- NA
+data_sem_miss[21:30, c(1:7, 9:14)] <- NA
+data_sem_miss[31:40, c(1:10, 12:14)] <- NA
+data_sem_miss[41:50, ] <- NA
+
+mod <-
+"
+f1 =~ x01 + x02 + x03
+f2 =~ x04 + x05 + x06 + x07
+f3 =~ x08 + x09 + x10
+f4 =~ x11 + x12 + x13 + x14
+f2 ~ a2*f1
+f3 ~ a3*f1
+f4 ~  b2*f2 + b3*f3 + cp*f1
+a2b2 := a2 * b2
+a3b3 := a3 * b3
+"
+
+# The warning is expected
+fit <- sam(
+  model = mod,
+  data = data_sem_miss,
+  missing = "fiml",
+  warn = FALSE
+)
+
+# The warning is expected
+fit2 <- sem(
+  model = mod,
+  data = data_sem_miss,
+  missing = "fiml",
+  warn = FALSE
+)
+
+expect_identical(lavInspect(fit),
+                 lavInspect(fit2))
+
+expect_identical(lavNames(fit, type = "eqs.y"),
+                 lavNames(fit2, type = "eqs.y"))
+
+expect_identical(lavNames(fit, type = "eqs.x"),
+                 lavNames(fit2, type = "eqs.x"))
+
+expect_identical(names(coef(fit)),
+                 names(coef(fit2)))
+
+expect_true(is.numeric(lavInspect(fit, "timing")$total))
+
+pt <- parameterTable(fit)
+pt2 <- parameterTable(fit2)
+
+pt_sorted <- pt[order(pt$lhs, pt$op, pt$rhs), ]
+pt2_sorted <- pt2[order(pt2$lhs, pt2$op, pt2$rhs), ]
+expect_equal(pt_sorted$lhs,
+             pt2_sorted$lhs)
+expect_equal(pt_sorted$rhs,
+             pt2_sorted$rhs)
+expect_equal(pt_sorted$op,
+             pt2_sorted$op)
+
+opt <- lavInspect(fit, "options")
+opt2 <- lavInspect(fit2, "options")
+
+setdiff(names(opt2), names(opt))
+
+expect_equal(lavaan::lavTech(fit, "ngroups"),
+             1)
+
+expect_equal(lavaan::lavTech(fit, "group.label"),
+             character(0))
+
+expect_identical(pt$free,
+                 pt2$free)
+
+expect_equal(lavNames(fit, "ov"),
+             lavNames(fit2, "ov"))
+
+expect_equal(lavNames(fit, "lv"),
+             lavNames(fit2, "lv"))
+
+expect_equal(lavNames(fit, "ov.ind"),
+             lavNames(fit2, "ov.ind"))
+
+expect_equal(rownames(lavInspect(fit, "cov.all")),
+             rownames(lavInspect(fit2, "cov.all")))
+
+expect_identical(lavaan::lavInspect(fit, "mean.lv"),
+                 lavaan::lavInspect(fit2, "mean.lv"))
+
+expect_identical(names(lavaan::lavInspect(fit, "mean.ov")),
+                 names(lavaan::lavInspect(fit2, "mean.ov")))
+
+expect_identical(lavaan::lavTech(fit, what = "post.check"),
+                 lavaan::lavTech(fit2, what = "post.check"))
+
+expect_identical(lavaan::lavTech(fit, what = "converged"),
+                 lavaan::lavTech(fit2, what = "converged"))
+
+expect_identical(lavaan::lavInspect(fit, "data"),
+                 lavaan::lavInspect(fit2, "data"))
+
+expect_identical(lavaan::lav_partable_labels(pt),
+                 lavaan::lav_partable_labels(pt2))
+
+expect_identical(lavaan::lavInspect(fit, "empty.idx"),
+                 lavaan::lavInspect(fit2, "empty.idx"))
+
+expect_identical(lavaan::lavInspect(fit, "meanstructure"),
+                 lavaan::lavInspect(fit2, "meanstructure"))
+
+expect_identical(colnames(lavaan::lavInspect(fit, "vcov")),
+                 colnames(lavaan::lavInspect(fit2, "vcov")))
+
+expect_identical(lavaan::lavTech(fit, "fixed.x"),
+                 lavaan::lavTech(fit2, "fixed.x"))
+
+expect_identical(lavaan::lavTech(fit, "ntotal"),
+                 lavaan::lavTech(fit2, "ntotal"))
+
+expect_identical(lavaan::lavTech(fit, "norig"),
+                 lavaan::lavTech(fit2, "norig"))
+
+expect_identical(lavaan::lavTech(fit, "pattern"),
+                 lavaan::lavTech(fit2, "pattern"))
+
+expect_identical(lavaan::lavTech(fit, "options")$test,
+                 lavaan::lavTech(fit2, "options")$test)
+
+fit@internal$sam.struc.fit
+fit@internal$sam.mm.rel
+fit@t@internal$sam.mm.table
+fit@internal$sam.method
+
+})
+
 test_that("SAM: Indirect effects", {
 
 # Test when functions will SAM
