9da5f3a2b9c05bff5dcdd81b344ffd4b2a033279
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-06-28 23:55:40 +0800

    Draft mod_lm
    build_site() passed

--- rebuild_vignettes.R
+++ rebuild_vignettes.R
@@ -26,6 +26,7 @@ knitr::knit("q_mediation_sem_indicators_parallel.Rmd.original", output = "q_medi
 knitr::knit("q_mediation_sem_indicators_arbitrary.Rmd.original", output = "q_mediation_sem_indicators_arbitrary.Rmd")
 knitr::knit("mome_sam.Rmd.original", output = "mome_sam.Rmd")
 knitr::knit("mo_sam.Rmd.original", output = "mo_sam.Rmd")
+knitr::knit("mo_lm.Rmd.original", output = "mo_lm.Rmd")
 pkgdown::build_articles("../../"); pkgdown::preview_site("../../")
 
 setwd(base_dir)
