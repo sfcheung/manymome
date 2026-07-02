1da76d0c671c73b04f7d8d04931d3d72c161d8ae
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-07-03 00:11:40 +0800

    Add mo_lm_cat_num_2w

--- rebuild_vignettes.R
+++ rebuild_vignettes.R
@@ -30,6 +30,7 @@ knitr::knit("mo_lm.Rmd.original", output = "mo_lm.Rmd")
 knitr::knit("mo_lm_2x1w.Rmd.original", output = "mo_lm_2x1w.Rmd")
 knitr::knit("mo_lm_2x2w.Rmd.original", output = "mo_lm_2x2w.Rmd")
 knitr::knit("mo_lm_cat_2w.Rmd.original", output = "mo_lm_cat_2w.Rmd")
+knitr::knit("mo_lm_cat_num_2w.Rmd.original", output = "mo_lm_cat_num_2w.Rmd")
 pkgdown::build_articles("../../"); pkgdown::preview_site("../../")
 
 setwd(base_dir)
